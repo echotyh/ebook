09324e7573e19b4c005c69d7e94708ef16ad3cf4
Sun Jianwei <327021593@qq.com>
2016-06-07 18:25:36 +0800

    收藏完成

---
+++ src/com/xiaotangbao/ebook/dao/CollectionDao.java
@@ -0,0 +1,16 @@
+package com.xiaotangbao.ebook.dao;
+
+/**
+ * 收藏表
+ *
+ * @author SunJianwei<327021593@qq.com>
+ * @date 16-6-7 17:10
+ */
+public class CollectionDao extends BaseDao {
+
+    public CollectionDao() {
+        db      = "ebook";
+        table   = "collection";
+    }
+
+}
